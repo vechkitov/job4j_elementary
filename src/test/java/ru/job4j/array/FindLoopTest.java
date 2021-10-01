8d3967fa92312d2ebc4f43e17006d9784f6b9c20
vechkitov <vechkitov@bk.ru>
2021-10-01 20:07:10 +0200

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -22,4 +22,48 @@ public class FindLoopTest {
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void whenElementNotBetweenStartAndFinish() {
+        int[] data = new int[] {5, 10, 3, 2, 4};
+        int el = 5;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenElementBetweenStartAndFinish() {
+        int[] data = new int[] {5, 10, 3, 2, 4};
+        int el = 3;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenElementOnStartIndex() {
+        int[] data = new int[] {5, 10, 3, 2, 4};
+        int el = 10;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenElementOnFinishIndex() {
+        int[] data = new int[] {5, 10, 3, 2, 4};
+        int el = 2;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
 }
