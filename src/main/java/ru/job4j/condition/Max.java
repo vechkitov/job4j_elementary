051cc964aa868cb7773f5096425f71a148ed6248
vechkitov <vechkitov@bk.ru>
2021-10-09 09:16:46 +0200

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,4 +6,14 @@ public class Max {
         int result = left > right ? left : right;
         return result;
     }
+
+    public static int max(int one, int two, int three) {
+        int result = max(max(one, two), three);
+        return result;
+    }
+
+    public static int max(int one, int two, int three, int four) {
+        int result = max(max(one, two), max(three, four));
+        return result;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,99 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void whenMax1And2And3Then3() {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int actual = Max.max(one, two, three);
+        int expected = 3;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax3And1And2Then3() {
+        int one = 3;
+        int two = 1;
+        int three = 2;
+        int actual = Max.max(one, two, three);
+        int expected = 3;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax2And3And1Then3() {
+        int one = 2;
+        int two = 3;
+        int three = 1;
+        int actual = Max.max(one, two, three);
+        int expected = 3;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax3And3And3Then3() {
+        int one = 3;
+        int two = 3;
+        int three = 3;
+        int actual = Max.max(one, two, three);
+        int expected = 3;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax1And2And3And4Then4() {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int actual = Max.max(one, two, three, four);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax4And1And2And3Then4() {
+        int one = 4;
+        int two = 1;
+        int three = 2;
+        int four = 3;
+        int actual = Max.max(one, two, three, four);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax3And4And1And2Then4() {
+        int one = 3;
+        int two = 4;
+        int three = 1;
+        int four = 2;
+        int actual = Max.max(one, two, three, four);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax2And3And4And1Then4() {
+        int one = 2;
+        int two = 3;
+        int three = 4;
+        int four = 1;
+        int actual = Max.max(one, two, three, four);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void whenMax4And4And4And4Then4() {
+        int one = 4;
+        int two = 4;
+        int three = 4;
+        int four = 4;
+        int actual = Max.max(one, two, three, four);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
 }
